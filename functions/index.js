7d9d95b96d96c51930eacd1cb6f4f9caebac7957
Jehan <jehan.tremback@gmail.com>
2018-02-23 10:45:22 -0800

    refactor js and add api keys etc

--- functions/index.js
+++ functions/index.js
@@ -1,80 +1,95 @@
 // @ts-check
 
-const firebaseAdmin = require('firebase-admin')
-const firebaseFunc = require('firebase-functions')
-const nodeGeocoder = require('node-geocoder')
-const rp = require('request-promise')
+const firebaseAdmin = require("firebase-admin");
+const firebaseFunc = require("firebase-functions");
+const nodeGeocoder = require("node-geocoder");
+const rp = require("request-promise");
 
 firebaseAdmin.initializeApp(firebaseFunc.config().firebase);
 
 var options = {
-	provider: 'google',
-	httpAdapter: 'https', 
-	apiKey: "AIzaSyA0Q7m1gULd67FSmRGaoP6UUtV-zlmMcJc",
-	formatter: null 
- };
+  provider: "google",
+  httpAdapter: "https",
+  apiKey: "AIzaSyAeDFgQ0YljZCLNcC1QtkgQHqhBcCUIWDE",
+  formatter: null
+};
 
 var geocoder = nodeGeocoder(options);
 
-exports.submit = firebaseFunc.https.onRequest((req, res) => {
-	const recaptchaResponse = req.body["g-recaptcha-response"];
+var submitLogic = active => (req, res) => {
+  const recaptchaResponse = req.body["g-recaptcha-response"];
 
-	const emailAddr = req.body["user_email_input"];
-	const firstName = req.body["user_fname_input"];
-	const lastName = req.body["user_lname_input"];
-	const country = req.body["user_country_menu"];
-	const city = req.body["user_city_input"];
-	const zipCode = req.body["user_zip_code_input"];
+  const emailAddr = req.body["user_email_input"];
+  const firstName = req.body["user_fname_input"];
+  const lastName = req.body["user_lname_input"];
+  const country = req.body["user_country_menu"];
+  const city = req.body["user_city_input"];
+  const zipCode = req.body["user_zip_code_input"];
 
-	const address = city + " " + zipCode + " " + country;
-	
-	rp({
-		uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
-		method: 'POST',
-		formData: {
-			 secret: '6LeopD8UAAAAALTKnD0jUog0tmE4Xvm_ofL128JM',
-			 response: recaptchaResponse
-		},
-		json: true
-	}).then(result => {
-		if (result.success) {
-			geocoder.geocode(address, function (err, geoCoderResult) {
-				firebaseAdmin.database().ref("Country/" + country).push().set({
-					Metadata: {
-						Timestamp: firebaseAdmin.database.ServerValue.TIMESTAMP
-					},
-					User_Information: {
-						First_Name: firstName,
-						Last_Name: lastName,
-						Email: emailAddr
-					},
-					User_Location: {
-						City: city,
-						Zip_Postal_Code: zipCode,
-						Country: country
-					},
-					GPS_Coordinates: {
-						Latitude: geoCoderResult[0].latitude,
-						Longitude: geoCoderResult[0].longitude
-					}
-				});
+  const address = city + " " + zipCode + " " + country;
 
-				firebaseAdmin.database().ref("Markers/").push().set({
-					Metadata: {
-						Timestamp: firebaseAdmin.database.ServerValue.TIMESTAMP
-					},
-					GPS_Coordinates: {
-						Latitude: geoCoderResult[0].latitude,
-						Longitude: geoCoderResult[0].longitude
-					}
-				});
+  rp({
+    uri: "https://recaptcha.google.com/recaptcha/api/siteverify",
+    method: "POST",
+    formData: {
+      secret: "6LcoCkgUAAAAAKsAOinoXHlqChcFRolbfTGY3eJ-",
+      response: recaptchaResponse
+    },
+    json: true
+  })
+    .then(result => {
+      if (result.success) {
+        geocoder.geocode(address, function(err, geoCoderResult) {
+          firebaseAdmin
+            .database()
+            .ref("Country/" + country)
+            .push()
+            .set({
+              Metadata: {
+                Timestamp: firebaseAdmin.database.ServerValue.TIMESTAMP
+              },
+              User_Information: {
+                First_Name: firstName,
+                Last_Name: lastName,
+                Email: emailAddr
+              },
+              User_Location: {
+                City: city,
+                Zip_Postal_Code: zipCode,
+                Country: country
+              },
+              Active: active,
+              GPS_Coordinates: {
+                Latitude: geoCoderResult[0].latitude,
+                Longitude: geoCoderResult[0].longitude
+              }
+            });
 
-				res.end("Recaptcha verification successful.")
-			});
-		} else {
-			res.status(500).end("Recaptcha verification failed.")
-		}
-	}).catch(reason => {
-		res.end("Recaptcha request failed.")
-	})
-})
+          firebaseAdmin
+            .database()
+            .ref("Markers/")
+            .push()
+            .set({
+              Metadata: {
+                Timestamp: firebaseAdmin.database.ServerValue.TIMESTAMP
+              },
+              Active: active,
+              GPS_Coordinates: {
+                Latitude: geoCoderResult[0].latitude,
+                Longitude: geoCoderResult[0].longitude
+              }
+            });
+
+          res.end("Recaptcha verification successful.");
+        });
+      } else {
+        res.status(500).end("Recaptcha verification failed.");
+      }
+    })
+    .catch(reason => {
+      res.end("Recaptcha request failed.");
+    });
+};
+
+exports.submit = firebaseFunc.https.onRequest(submitLogic(false));
+exports.submitActive = firebaseFunc.https.onRequest(submitLogic(true));
